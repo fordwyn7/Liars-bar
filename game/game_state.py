b3c876acabc4acf8ce740e76d7567d4b08958ce9
iqboljon7 <rsfordwyn@gmail.com>
2025-01-28 10:08:00 +0500

    turnir bug fixing

--- game/game_state.py
+++ game/game_state.py
@@ -521,7 +521,7 @@ async def handle_continue_or_liar(callback_query: types.CallbackQuery):
                 cur_round = int(get_current_round_number(tournament_id))
                 plrs = get_users_in_round(tournament_id, cur_round)
                 nopir = int(get_number_of_groups_in_round(tournament_id, cur_round))
-                await bot.send_message(chat_id=1155076760, text=f"{plrs, cur_round, get_number_of_winners(tournament_id, cur_round)} check 1")
+                await bot.send_message(chat_id=1155076760, text=f"{plrs}, {cur_round}, {get_number_of_winners(tournament_id, cur_round)} check 1")
                 
                 if int(get_number_of_winners(tournament_id, cur_round)) == nopir:
                     notify_round_results(tournament_id, cur_round)
