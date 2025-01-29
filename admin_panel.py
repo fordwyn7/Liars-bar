ff5df2daa8afcb3ad3807f6b9b7b40e3e733ea14
iqboljon7 <rsfordwyn@gmail.com>
2025-01-29 15:37:57 +0500

    tournament registration fix

--- admin_panel.py
+++ admin_panel.py
@@ -1224,9 +1224,8 @@ async def cancel_withdraw_queer(callback_query: types.CallbackQuery, state: FSMC
 async def watch_results_f(message: types.Message):
     result = ""
     tournament = get_ongoing_tournaments()
-    await message.answer(f"{tournament}")
     tournament_id = tournament[0]["name"]
-    current_round = get_current_round_number()
+    current_round = get_current_round_number(tournament_id)
     for i in range(1, current_round + 1):
         result += get_round_results(tournament_id, i) + "\n"
     await message.answer(result)
