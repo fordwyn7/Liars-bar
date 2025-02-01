dc99572c4fbfa6d9f4b2598ff1bb4bf1188fa4a3
iqboljon7 <rsfordwyn@gmail.com>
2025-02-01 18:18:15 +0500

    feaf

--- states/test.py
+++ states/test.py
@@ -1,1 +1,14 @@
-
+nmd = 5
+groups = []
+participants = [i for i in range(1, 22)]
+# for i in range(0, nmd):
+#     groups.append(participants[:4])
+#     participants = participants[4:]
+# if participants:
+#     groups.append(participants)
+for i in range(0, nmd - 1):
+    groups.append(participants[: 4])
+    participants = participants[4 :]
+groups.append(participants[:2])
+groups.append(participants[2:])
+print(groups)
